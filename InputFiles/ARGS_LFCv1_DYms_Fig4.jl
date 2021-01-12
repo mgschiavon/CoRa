3e1a5ae125ba726af0b82b0cf4ccb0660b37547f
mgschiavon <mgschiavon@users.noreply.github.com>
2021-01-12 11:15:25 -0800

    Revert "Input files for Natural Example figure"
    This reverts commit 993148d4713fd061aa036dddcb4e4100b5f91403.

--- InputFiles/ARGS_LFCv1_DYms_Fig4.jl
+++
@@ -1,8 +0,0 @@
-# Parameters to optimize
-pN = Dict([
-			:kX =>	collect(-3:1:3),
-			:kY =>	collect(-3:1:3),
-			:kZ =>	collect(-3:1:3),
-			:n  =>	collect(0:(1/3):2),
-			:oY =>	collect(-3:1:3)
-			]);
