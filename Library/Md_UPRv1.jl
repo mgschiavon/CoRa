17f27cc909e2ef08a6a187c2f1297aaa270a4336
mgschiavon <mgschiavon@users.noreply.github.com>
2021-02-18 09:17:12 -0800

    Adding I synthesis/degradation
    ...to get a given steady state regardless of initial conditions.

--- Library/Md_UPRv1.jl
+++ Library/Md_UPRv1.jl
@@ -15,28 +15,28 @@ module mm
 		dUB  = (cB * U * B) - (gUB * UB) - (cD * UB) + (cE * E * UdB) - (gB * UB) - (gF * UB)
 		dUd  = - (cB * Ud * B) + (gUB * UdB) + (cD * U) - (cE * E * Ud) + (gB * UdB)
 		dUdB = (cB * Ud * B) - (gUB * UdB) + (cD * UB) - (cE * E * UdB) - (gB * UdB)
-		dI   = - (cB * B * I) - (cA * (U + Ud + UdB) * I) + (gIB * IB) + (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
-		dIB  = (cB * B * I) - (gIB * IB)
-		dIa  = (cA * (U + Ud + UdB) * I) - (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
+		dI   = mI - (gI * I) - (cB * B * I) - (cA * (U + Ud + UdB) * I) + (gIB * IB) + (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
+		dIB  = - (gI * IB) + (cB * B * I) - (gIB * IB)
+		dIa  = - (gI * Ia) + (cA * (U + Ud + UdB) * I) - (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
 		dHu  = bHu - (gHs * Hu) - (bHs * min(Ia,Hu))
 		dHs  = - (gHs * Hs) + (bHs * min(Ia,Hu))
 		dB   = (bB * (1 + (nB * (Hs ^ 2)/(a0 + (a1 * Hs) + (Hs ^ 2))))) - (gB * B) - (cB * U * B) + (gUB * UB) - (cB * Ud * B) + (gUB * UdB) + (gF * UB)
 		dE   = (bE * (1 + (nE * (Hs ^ 2)/(a0 + (a1 * Hs) + (Hs ^ 2))))) - (gE * E)
-	end sU cB gUB cD cE gB gF cA gIB gIA kI nI bHu gHs bHs bB nB a0 a1 gB bE nE gE uT;
+	end sU cB gUB cD cE gB gF mI gI cA gIB gIA kI nI bHu gHs bHs bB nB a0 a1 gB bE nE gE uT;
 	# ODE system without feedback
 	odeNF = @ode_def begin
 		dU   = sU - (cB * U * B) + (gUB * UB) - (cD * U) + (cE * E * Ud) + (gB * UB)
 		dUB  = (cB * U * B) - (gUB * UB) - (cD * UB) + (cE * E * UdB) - (gB * UB) - (gF * UB)
 		dUd  = - (cB * Ud * B) + (gUB * UdB) + (cD * U) - (cE * E * Ud) + (gB * UdB)
 		dUdB = (cB * Ud * B) - (gUB * UdB) + (cD * UB) - (cE * E * UdB) - (gB * UdB)
-		dI   = - (cB * B * I) - (cA * uT * I) + (gIB * IB) + (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
-		dIB  = (cB * B * I) - (gIB * IB)
-		dIa  = (cA * uT * I) - (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
+		dI   = mI - (gI * I) - (cB * B * I) - (cA * uT * I) + (gIB * IB) + (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
+		dIB  = - (gI * IB) + (cB * B * I) - (gIB * IB)
+		dIa  = - (gI * Ia) + (cA * uT * I) - (gIA * ((kI ^ nI)/((kI ^ nI) + (Ia ^ nI))) * Ia)
 		dHu  = bHu - (gHs * Hu) - (bHs * min(Ia,Hu))
 		dHs  = - (gHs * Hs) + (bHs * min(Ia,Hu))
 		dB   = (bB * (1 + (nB * (Hs ^ 2)/(a0 + (a1 * Hs) + (Hs ^ 2))))) - (gB * B) - (cB * U * B) + (gUB * UB) - (cB * Ud * B) + (gUB * UdB) + (gF * UB)
 		dE   = (bE * (1 + (nE * (Hs ^ 2)/(a0 + (a1 * Hs) + (Hs ^ 2))))) - (gE * E)
-	end sU cB gUB cD cE gB gF cA gIB gIA kI nI bHu gHs bHs bB nB a0 a1 gB bE nE gE uT;
+	end sU cB gUB cD cE gB gF mI gI cA gIB gIA kI nI bHu gHs bHs bB nB a0 a1 gB bE nE gE uT;
 
 	# Define system's output (total Y):
 	function outFB(ss)
