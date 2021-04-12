e2f53c98664b3e66acfd698caf59156034e5c290
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-04-12 11:48:31 -0500

    Exploring possible typo in Pincus et al.
    IA inactivation function: F_IA * IA vs F_IA(IA)

--- Test_Pincus.jl
+++ Test_Pincus.jl
@@ -5,7 +5,7 @@ using Distributions
 using Plots
 
 cd("C:\\Users\\mgsch\\Dropbox (MGS-UCSF)\\MODEL - Quantifying feedback\\Paper\\CoRa\\")
-iARG = (mm = "UPRv1",  # Label for motif file
+iARG = (mm = "UPRv4",  # Label for motif file
      ex = "Ex01",      # Label for parameters file
      pp = :cD,         # Label for perturbation type
      ax = :cD,         # Label for condition/environment
@@ -20,12 +20,14 @@ pO = copy(p);
 
 # Find initial conditions as 0nM DTT steady state (i.e. pre-disturbance):
 p[:cD] = 0.0;
+pV = [p[i] for i in mm.odeFB.params];
 x0 = zeros(length(mm.odeFB.syms));
 x0[5] = 256;    # :I, I total = 256 mol
 x0[8] = 200;    # :Hu, basal Hac1 = 200 mol
-x0[10] = 430000;# :B, basal BiP = 430,000 mol
-ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hint=[:stiff],reltol=1e-4,callback=TerminateSteadyState());
-x0 = ss.u[end];
+x0[11] = 430000;# :B, basal BiP = 430,000 mol
+#ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hint=[:stiff],reltol=1e-4,callback=TerminateSteadyState());
+#x0 = ss.u[end];
+#x0 = [28000.0,372000.0,0.0,0.0,18.0,237.0,1.0,200.0,1.0,247.0,73000.0,1600.0,3.0e6];
 
 # Choose variable to plot:
 iS = 9;
