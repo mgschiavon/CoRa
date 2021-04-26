251a803fc7e21e427888028c6ea2d3f3013b0d9a
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-04-26 16:15:11 -0500

    typo 'alg_hints'
    I realized 'alg_hint=[:stiff]' was wrong (and then useless). It should be: 'alg_hints=[:stiff]'.

--- Test_Pincus.jl
+++ Test_Pincus.jl
@@ -6,7 +6,7 @@ using Plots
 
 cd("C:\\Users\\mgsch\\Dropbox (MGS-UCSF)\\MODEL - Quantifying feedback\\Paper\\CoRa\\")
 iARG = (mm = "UPRv5",  # Label for motif file
-     ex = "mutH",      # Label for parameters file
+     ex = "Ex01",      # Label for parameters file
      pp = :cD,         # Label for perturbation type
      ax = :cD,         # Label for condition/environment
      an = "ExSSs");    # Chose analysis type (Options: ExSSs, ExDyn, DYms, OptDY)
@@ -25,29 +25,17 @@ x0 = zeros(length(mm.odeFB.syms));
 x0[5] = 256;    # :I, I total = 256 mol
 x0[8] = p[:bHu]/p[:gHs];    # :Hu, basal Hac1 = 200 mol
 x0[11] = 430000;# :B, basal BiP = 430,000 mol
-#ss = solve(ODEProblem(mm.odeFB,x0,1e7,pV),alg_hint=[:stiff],reltol=1e-5,callback=TerminateSteadyState());
+#ss = solve(ODEProblem(mm.odeFB,x0,1e7,pV),alg_hints=[:stiff],reltol=1e-5,callback=TerminateSteadyState());
 #x0 = ss.u[end];
 ########## For UPRv5 | Ex01 ##########
-#ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hint=[:stiff]);
-#x0 = [28430.812810605, 372148.85954383,      0.0,      0.0,     18.133988230549,    237.36823020938,      0.6128894545444,    199.37654960166,      1.1036424751692,    247.5388675555,  73302.29924954,   1664.6746405859,      2994019.1377436,      1.1036424751692,    264.9378053966];
+#ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hints=[:stiff]);
+x0 = [28430.812810605, 372148.85954383,      0.0,      0.0,     18.133988230549,    237.36823020938,      0.6128894545444,    199.37654960166,      1.1036424751692,    247.5388675555,  73302.29924954,   1664.6746405859,      2994019.1377436,      1.1036424751692,    264.9378053966];
 ########## For UPRv5 | mutH ##########
-#ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hint=[:stiff]);
-x0 = [31449.290697777767
- 372148.8595413453
-      0.0
-      0.0
-     19.89902676170431
-    235.47213383742258
-      0.743947411157102
-    199.14055088050873
-      1.3396411963220092
-    243.62818590704651
-  66266.80291661603
-   1619.1904047976013
-      2.9122129582690676e6
-      1.3396411963220092
-    321.59109966476416];
-
+#ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hints=[:stiff]);
+#x0 = [31449.290697777767, 372148.8595413453,      0.0,      0.0,     19.89902676170431,    235.47213383742258,      0.743947411157102,    199.14055088050873,      1.3396411963220092,    243.62818590704651,  66266.80291661603,   1619.1904047976013,      2.9122129582690676e6,      1.3396411963220092,    321.59109966476416];
+########## For UPRv5 | mutI ##########
+#ss = solve(ODEProblem(mm.odeFB,x0,1e6,pV),alg_hints=[:stiff]);
+#x0 = [9938.6135044051, 372148.85954332,      0.0,      0.0,    253.124484807358,      0.0,      2.99062310503571,    195.09491637774,      5.3852756990812,    323.50330569012, 209691.618214591,   2548.19949027465,      4.5830926081760e6,      5.3852756990812,   1292.77655884681];
 # Choose variable to plot:
 iS = 15;
 
@@ -57,7 +45,7 @@ iS = 15;
 ### [0 - 9710.475 (1/s)]
 p[:cD] = 0.0;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
@@ -66,7 +54,7 @@ plot(ss.t/60,x,label=string("DTT = ",p[:cD]*5.0/9710.475," nM"),lw=3)
 
 p[:cD] = 9710.475*0.66/5;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
@@ -75,7 +63,7 @@ plot!(ss.t/60,x,label=string("DTT = ",p[:cD]*5.0/9710.475," nM"),lw=3)
 
 p[:cD] = 9710.475*1/5;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
@@ -84,7 +72,7 @@ plot!(ss.t/60,x,label=string("DTT = ",p[:cD]*5.0/9710.475," nM"),lw=3)
 
 p[:cD] = 9710.475*1.5/5;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
@@ -93,7 +81,7 @@ plot!(ss.t/60,x,label=string("DTT = ",p[:cD]*5.0/9710.475," nM"),lw=3)
 
 p[:cD] = 9710.475*2.2/5;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
@@ -102,7 +90,7 @@ plot!(ss.t/60,x,label=string("DTT = ",p[:cD]*5.0/9710.475," nM"),lw=3)
 
 p[:cD] = 9710.475*3.3/5;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
@@ -111,7 +99,7 @@ plot!(ss.t/60,x,label=string("DTT = ",p[:cD]*5.0/9710.475," nM"),lw=3)
 
 p[:cD] = 9710.475;
 pV = [p[i] for i in mm.odeFB.params];
-ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hint=[:stiff]);
+ss = solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff]);
 x = zeros(length(ss.u));
 for i in 1:length(ss.u)
     x[i] = ss.u[i][iS];
