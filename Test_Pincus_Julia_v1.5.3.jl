6fae38d1d204c32c66b82de2afb9482f44958a96
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-05-04 14:29:54 -0500

    More accurate DTT number of molecules
    As in Michael Chevalier's code
    (+ condensed code for plotting)

--- Test_Pincus_Julia_v1.5.3.jl
+++ Test_Pincus_Julia_v1.5.3.jl
@@ -42,127 +42,30 @@ x0 = ss.u[end];
 iS = 15;
 
 # Simulate and plot different perturbation values:
-### :cD - Enzymatic rate of disulfide bond breaking
-### [0.0015 (1/mol)(1/s)] x DTT concentration [0 - 6500 mol = 0-5 mM]:
-### [0 - 9710.475 (1/s)]
-p[:cD] = 0.0;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
+DTT = [0,0.6600,0.9900,1.4800,2.2200,3.5500,5.0000];    # DTT concentration
+### :cD - Enzymatic rate of disulfide bond breaking x DTT number of molecules in the ER
+for d in DTT
+    p[:cD] = p[:cD0] * (p[:mMc] * (d * p[:ERv]));
+    pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
+    ss = try
+            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
         catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
+            try
+                solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
+            catch
+                println("WARNING: Error in steady state calculation.")
+            end
+        end;
+    x = zeros(length(ss.u));
+    for i in 1:length(ss.u)
+        x[i] = ss.u[i][iS];
+    end
+    if d==0
+        plot(ss.t/60,x,label=string("DTT = ",d," mM"),lw=3,legend=:topleft)
+    else
+        plot!(ss.t/60,x,label=string("DTT = ",d," mM"),lw=3,legend=:topleft)
+    end
 end
-plot(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3,legend=:topleft)
-
-p[:cD] = 9710.475*0.66/5;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-        catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
-end
-plot!(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3)
-
-p[:cD] = 9710.475*1/5;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-        catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
-end
-plot!(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3)
-
-p[:cD] = 9710.475*1.5/5;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-        catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
-end
-plot!(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3)
-
-p[:cD] = 9710.475*2.2/5;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-        catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
-end
-plot!(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3)
-
-p[:cD] = 9710.475*3.3/5;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-        catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
-end
-plot!(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3)
-
-p[:cD] = 9710.475;
-pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.odeFB.sys.ps];
-ss = try
-        solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),alg_hints=[:stiff],isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-    catch
-        try
-            solve(ODEProblem(mm.odeFB,x0,240.0*60,pV),isoutofdomain=(u,p,t) -> any(x -> (x < 0), u));
-        catch
-            println("WARNING: Error in steady state calculation.")
-        end
-    end;
-x = zeros(length(ss.u));
-for i in 1:length(ss.u)
-    x[i] = ss.u[i][iS];
-end
-plot!(ss.t/60,x,label=string("DTT = ",round(100*p[:cD]*5.0/9710.475)/100," mM"),lw=3)
 
     xlabel!("Minutes")
     ylabel!(string(mm.odeFB.syms[iS]," molecules"))
