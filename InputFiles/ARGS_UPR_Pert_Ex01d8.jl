a71431cdc64a9fad3f92e0164a820ae33833fb32
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-06-14 17:27:54 -0500

    Exploring UPR dynamics
    Temporal analysis...

---
+++ InputFiles/ARGS_UPR_Pert_Ex01d8.jl
@@ -0,0 +1,7 @@
+# Perturbation details
+pert = (p   = iARG.pp,	# Parameter to be perturbed
+		d   = 1.05,		# Perturbation size (Delta rho)
+		c   = iARG.ax,	# Condition parameter
+		r   = [0,6],	# Range of conditions
+		s   = 0.01,		# Step
+		eps = 0.1);		# DY threshold
